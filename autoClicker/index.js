37475c1665054f42cdcaa3b45bfaeb6204af9456
Sobhan 01 <sobhan.kz01.dev@gmail.com>
2024-06-08 19:00:11 +0330

    Update index.js

--- autoClicker/index.js
+++ autoClicker/index.js
@@ -1,11 +1,20 @@
 
 
-const evt1 = new PointerEvent('pointerdown', { clientX: 150, clientY: 300 });
-const evt2 = new PointerEvent('pointerup', { clientX: 150, clientY: 300 });
 const energy = parseInt(document.getElementsByClassName("user-tap-energy")[0].getElementsByTagName("p")[0].textContent.split(" / ")[0]);
 var timeout = false
+Array.prototype.sample = function () {
+    return this[Math.floor(Math.random() * this.length)];
+}
 
 function fn() {
+    var clientX = [150, 170, 210,300,350,180,160,190,400,380];
+    var clientY = [300, 340, 400,280,450,320,310,355];
+
+
+
+
+    const evt1 = new PointerEvent('pointerdown', { clientX: clientX.sample(), clientY: clientY.sample() });
+    const evt2 = new PointerEvent('pointerup', { clientX: clientX.sample(), clientY: clientY.sample() });
     var energy = parseInt(document.getElementsByClassName("user-tap-energy")[0].getElementsByTagName("p")[0].textContent.split(" / ")[0]);
     if (energy > 25 && !timeout) {
         document.getElementsByClassName('user-tap-button')[0].dispatchEvent(evt1);
@@ -26,3 +35,4 @@ function fn() {
 }
 
 setInterval(fn, 80);
+
